5d02b6d1cce889db6c75bc0bf596d7974ef8b72c
Brian Miyaji <brian@themeboy.com>
2015-10-06 15:05:40 +1100

    Simplify script

--- js/sportspress-cricket.js
+++ js/sportspress-cricket.js
@@ -1,13 +1,10 @@
-jQuery.fn.sp_swap = function(b){ 
-    b = jQuery(b)[0]; 
-    var a = this[0]; 
-    var t = a.parentNode.insertBefore(document.createTextNode(''), a); 
-    b.parentNode.insertBefore(a, b); 
-    t.parentNode.insertBefore(b, t); 
-    t.parentNode.removeChild(t); 
-    return this; 
-};
-
-jQuery(document).ready(function($){
-	$('.sp-template-event-performance-team-0-position-1').sp_swap('.sp-template-event-performance-team-1-position-1');
-});
+(function($) {
+	var a = $('.sp-template-event-performance-team-0-position-1')[0];
+	var b = $('.sp-template-event-performance-team-1-position-1')[0];
+	if ( undefined !== a && undefined !== b ) {
+		var t = a.parentNode.insertBefore(document.createTextNode(''), a); 
+		b.parentNode.insertBefore(a, b); 
+		t.parentNode.insertBefore(b, t); 
+		t.parentNode.removeChild(t); 
+	}
+})(jQuery);
