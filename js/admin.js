4f99b9ef92bd0cb8b254d91484441004889b6414
Brian Miyaji <brian@themeboy.com>
2016-02-24 13:36:11 +1100

    Remove unused JS

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,4 @@
 jQuery(document).ready(function($){
-	// Switch performance tables
-	$(".sp-performance-table-bar a").click(function() {
-		i = $(this).closest("li").index();
-		$(this).addClass("current").closest("li").siblings().find("a").removeClass("current");
-		$(this).closest(".sp-performance-table-bar").siblings(".sp-data-table-container").hide().eq(i).show();
-	});
-
 	// Hide substitute dropdown
 	$(".sp-status-selector select:first-child").unbind("change").siblings().hide();
 });
