a406cb9d468c7f35af08bc3ecbfa518fbd46a24d
Brian Miyaji <brian@themeboy.com>
2016-03-15 20:51:08 +1100

    Prevent output of wickets if none left

--- sportspress-for-cricket.php
+++ sportspress-for-cricket.php
@@ -339,7 +339,7 @@ class SportsPress_Cricket {
 		if ( false === $val ) {
 			$val = reset( $team_results );
 		}
-		if ( isset( $val ) && ! is_array( $val ) && '' !== $val ) {
+		if ( isset( $val ) && ! is_array( $val ) && '' !== $val && '10' !== $val ) {
 			$delimiter = get_option( 'sportspress_event_logos_results_delimiter', '/' );
 			$reverse = get_option( 'sportspress_event_logos_reverse_results_format', 'no' );
 			if ( 'yes' == $reverse ) {
